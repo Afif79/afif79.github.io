6d479d104391acbabcd5a820a958f5fe72699a4a
AFIF <46858532+Afif79@users.noreply.github.com>
2020-06-17 20:59:48 +0530

    bio updated

--- javascript/app.js
+++ javascript/app.js
@@ -21,3 +21,18 @@ let trans = () => {
         document.documentElement.classList.remove('transition')
     }, 1000)
 }
+
+
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+console.log('age: ' + getAge("2000/01/08"));
+var y = getAge("2000/01/08");
+document.getElementById("myAge").innerHTML = y; 
